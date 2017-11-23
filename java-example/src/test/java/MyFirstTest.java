ff8177a41fefe313d70233d34bca5513e85d34f2
Michael Tishin <tishinmixa@gmail.com>
2017-11-23 13:02:51 +0300

    Initial commit

---
+++ java-example/src/test/java/MyFirstTest.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 5);
+    }
+
+    @Test
+    public  void myFirstTest() {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin" + Keys.RETURN);
+        driver.findElement(By.name("password")).sendKeys("admin" + Keys.ENTER);
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
+
+}
