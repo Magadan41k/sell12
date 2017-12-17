b139b684c0b59ff6aeaca40cfb42533897ad2311
Michael Tishin <tishinmixa@gmail.com>
2017-12-17 17:05:41 +0300

    1
    1

--- java-example/src/test/java/CountriesGeo.java
+++ java-example/src/test/java/CountriesGeo.java
@@ -5,17 +5,13 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class CountriesGeo {
